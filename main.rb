d65c0065151d8d4aff38a162f33662c3617e0652
MasayukiYasukawa <masayuki.yasukawa@tokyo.supersoftware>
2019-07-16 19:03:35 +0900

    実行ファイル名変更、ドキュメント削除、実行ファイルのクラス継承を削除

---
+++ main.rb
@@ -0,0 +1,18 @@
+require './car'
+require './dealer'
+require './honda'
+require './nissan'
+require './ferrari'
+
+rand(0..10).times do
+	Honda.new("Honda", rand(151..200))
+	Nissan.new("Nissan", rand(100..150))
+	Ferrari.new("Ferrari", rand(300..400))
+end
+
+unless Dealer.total_count == 0
+	puts "#{Dealer.total_count}台の合計金額#{Dealer.total_price}万円"
+	puts "#{Dealer.total_count}台の平均金額#{Dealer.average_price}万円"
+else
+	puts "登録された車がありません。" 		
+end
